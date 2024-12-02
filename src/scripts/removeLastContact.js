ebb4399a2e9e88d126828d85b1083a5c272a81d4
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-12-02 18:00:01 +0100

    homework

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,10 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const removeLast = await readContacts();
+  removeLast.pop();
+  await writeContacts(removeLast);
+};
 
 removeLastContact();
